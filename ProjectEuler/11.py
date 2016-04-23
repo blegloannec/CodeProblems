ddb23aae13bd7183fd000ea63b54e39e03d3adc6
blegloannec <blg@gmx.com>
2016-04-23 23:54:30 +0200

    ez hr+

--- ProjectEuler/11.py
+++ ProjectEuler/11.py
@@ -6,14 +6,17 @@ def problem11():
     pmax = 0
     for i in range(n-3):
         for j in range(n-3):
-            p = M[i][j]*M[i+1][j]*M[i+2][j]*M[i+3][j]
-            pmax = max(p,pmax)
-            p = M[i][j]*M[i][j+1]*M[i][j+2]*M[i][j+3]
-            pmax = max(p,pmax)
-            p = M[i][j]*M[i+1][j+1]*M[i+2][j+2]*M[i+3][j+3]
-            pmax = max(p,pmax)
-            p = M[i+3][j]*M[i+2][j+1]*M[i+1][j+2]*M[i][j+3]
-            pmax = max(p,pmax)
+            if i<n-3:
+                p = M[i][j]*M[i+1][j]*M[i+2][j]*M[i+3][j]
+                pmax = max(p,pmax)
+            if j<n-3:
+                p = M[i][j]*M[i][j+1]*M[i][j+2]*M[i][j+3]
+                pmax = max(p,pmax)
+            if i<n-3 and j<n-3:
+                p = M[i][j]*M[i+1][j+1]*M[i+2][j+2]*M[i+3][j+3]
+                pmax = max(p,pmax)
+                p = M[i+3][j]*M[i+2][j+1]*M[i+1][j+2]*M[i][j+3]
+                pmax = max(p,pmax)
     print pmax
 
 problem11()
--- ProjectEuler/15.py
+++ ProjectEuler/15.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 
+# Prog dyn naive implementee ici...
+# Mais un tel chemin est parfaitement defini par un multi-ensemble
+# a N elements choisis parmi les M+1 bordures verticales de la
+# grille, definissant les N emplacements ou l'on descend.
+# Il s'agit du nb de combinaisons avec repetitions de taille N
+# d'un ensemble de taille M+1, soit N parmi N+M.
+
 def problem15():
     n = 20+1
     M = [[1 for _ in range(n)] for _ in range(n)]
