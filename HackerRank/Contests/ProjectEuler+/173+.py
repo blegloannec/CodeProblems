8a13701a277befa4fbfa6a1a126d91fced546747
blegloannec <blg@gmx.com>
2017-02-26 06:09:24 +0100

    hr pe+ 173 (doc)

--- HackerRank/Contests/ProjectEuler+/173+.py
+++ HackerRank/Contests/ProjectEuler+/173+.py
@@ -2,10 +2,9 @@
 
 from math import sqrt
 
-# pout M fixe, on cherche tous les couples (a,b)
-# tels que a^2-b^2 <= M avec 0 < b < a
-# et b = a-2k, ie b de meme parite que a
-# a+b = 2l pair
+# on cherche tous les couples (a,b) tels que
+# a^2-b^2 <= M avec 0 < b < a et b = a-2k
+# i.e. b de meme parite que a, a+b = 2l pair
 # a^2-b^2 = (a-b)(a+b) = 4kl <= M avec 1<k<l
 # donc k < sqrt(M/4)
 
