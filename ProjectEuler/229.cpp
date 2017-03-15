e57583cbd970514c3f0df424efde901020ee10f8
blegloannec <blg@gmx.com>
2017-03-15 22:17:22 +0100

    pe229 (small corr.)

--- ProjectEuler/229.cpp
+++ ProjectEuler/229.cpp
@@ -13,13 +13,19 @@ int main() {
   for (long long a=1; a<S; ++a)
     for (long long b=1; b*b<N-a*a; ++b) {
       long long n = a*a+b*b;
-      if (n<N) A[n] = true;
-      n = a*a+2*b*b;
-      if (n<N) B[n] = true;
-      n = a*a+3*b*b;
-      if (n<N) C[n] = true;
-      n = a*a+7*b*b;
-      if (n<N) D[n] = true;
+      if (n<N) {
+	A[n] = true;
+	n = a*a+2*b*b;
+	if (n<N) {
+	  B[n] = true;
+	  n = a*a+3*b*b;
+	  if (n<N) {
+	    C[n] = true;
+	    n = a*a+7*b*b;
+	    if (n<N) D[n] = true;
+	  }
+	}
+      }
     }
   int cpt = 0;
   for (int a=1; a<N; ++a)
