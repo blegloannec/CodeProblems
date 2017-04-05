e918d49a9089d730d2a73a493c87582bd6828c1f
blegloannec <blg@gmx.com>
2017-04-05 23:43:53 +0200

    cg

--- CodinGame/Community/two_egg_pb.py
+++ CodinGame/Community/two_egg_pb.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+from math import sqrt,ceil
+
 n = int(input())
 
+# DP
 P = [-1 for _ in range(n+1)]
 P[0] = 0
 P[1] = 0
@@ -15,4 +18,15 @@ def wc(n):
     P[n] = res
     return res
 
-print(wc(n))
+#print(wc(n))
+
+
+# Closed formula
+# x(x+1) = n
+# x^2 + x - 2n = 0
+# -1+sqrt(1+8n) / 2
+
+def formula(n):
+    return int(ceil((-1+sqrt(1+8*n))/2))
+
+print(formula(n))
