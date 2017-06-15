2080bd95c8496a5d922d362e3346ed9e66c99761
blegloannec <blg@gmx.com>
2017-06-15 23:29:20 +0200

    misc

--- Misc/kmp.cpp
+++ Misc/kmp.cpp
@@ -3,32 +3,28 @@
 #include <string>
 using namespace std;
 
-typedef vector<int> vint;
-
-vector<int> prefs(string B) {
-  vint P = vint(B.size(),0);
+void prefs(string B, vector<int> &P) {
+  P.resize(B.size()+1);
   P[0] = -1;
   int k = -1;
-  for (int i=1; i<(int)B.size(); ++i) {
+  for (int i=1; i<=(int)B.size(); ++i) {
     while (k>=0 && B[i-1]!=B[k]) k = P[k];
     ++k;
     P[i] = k;
   }
-  return P;
 }
 
-vint kmp(string A, string B) {
-  vint pos;
-  vint P = prefs(B);
+vector<int> kmp(string A, string B) {
+  vector<int> pos;
+  vector<int> P;
+  prefs(B,P);
   int k = 0;
   for (int i=0; i<(int)A.size(); ++i) {
     while (k>=0 && A[i]!=B[k]) k = P[k];
     ++k;
     if (k==(int)B.size()) {
       pos.push_back(i-(int)B.size()+1);
-      // reset pour tous les matchs
-      i = i-(int)B.size()+1;
-      k = 0;
+      k = P[k];
     }
   }
   return pos;
@@ -40,11 +36,11 @@ int main() {
   for (int c=0; c<C; ++c) {
     string A,B;
     cin >> A >> B;
-    vint res = kmp(A,B);
+    vector<int> res = kmp(A,B);
     if (res.size()==0)
       cout << "No Match!" << endl;
     else {
-      for (vint::iterator it=res.begin(); it!=res.end(); ++it)
+      for (vector<int>::iterator it=res.begin(); it!=res.end(); ++it)
 	cout << *it << ' ';
       cout << endl;
     }
