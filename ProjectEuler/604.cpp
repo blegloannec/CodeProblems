b8d708605b8fabe625da518518ddeb33f882d0a2
blegloannec <blg@gmx.com>
2017-07-07 00:24:50 +0200

    pe (doc)

--- ProjectEuler/604.cpp
+++ ProjectEuler/604.cpp
@@ -6,13 +6,26 @@ using namespace std;
   par exemple pour N = 11, on peut prendre les pentes
   successives 1/3, 1/2, 1/1, 3/2, 2/1, 3/1
   ce qui donne 7 points de l'optimum partant de (0,0)
-  pour F(n), il s'agit de trouver une plus grande sequence
-  strictement croissante de fractions irreductibles telles que
+  pour F(n), il s'agit de trouver un plus grand ensemble
+  de fractions irreductibles distinctes telles que
   la somme des numerateurs (deplacement vertical) et la somme
   des denominateurs (deplacement horizontal) soient <= N
+  (pour construire la "courbe" correspondante, on trierait les fractions)
   pour l'exemple 1+1+1+3+2+3 = 11 et 3+2+1+2+1+1 = 10
   la sequence partant de (0,0) aboutit en (10,11) <= (11,11)
-  generateur des diviseurs (a partir des decomp)
+  
+  une fraction a/b contribue d'autant moins que a+b est faible, on
+  considere donc les fractions dans cet ordre
+  pour x fixe, si l'on connait C(x) le nb de fractions irreductibles a/b
+  avec a+b = x, alors leur contribution (verticale/horizontale)
+  totale est C(x)*x/2
+  des lors, si l'on sait calculer efficacement C(), on peut traiter
+  toutes ces fractions d'un coup ce qui permet d'arriver tres vite
+  au x limite pour lequel on ajoute les fractions une a une jusqu'a
+  arriver a N
+  essentiellement, c'est un algo glouton tres accelere
+
+  runs in 1s with -03
 */
 
 typedef long long ll;
@@ -69,6 +82,7 @@ int gcd(int a, int b) {
 }
 
 int main() {
+  // Calcul de C()
   sieve_smallest_factor();
   vector<int> C(N,0);
   // C[i] = nb de couples (a,b) tq i = a+b et gcd(a,b) = 1
@@ -84,6 +98,7 @@ int main() {
     for (vector<int>::iterator id=D.begin(); id!=D.end(); ++id)
       if (0<*id && *id<i) C[i] -= C[*id];
   }
+  // Glouton accelere
   ll s = 1, ab = 3, f = 1;
   while (s<=n) {
     if (s+(C[ab]*ab)/2<=n) {
@@ -92,7 +107,7 @@ int main() {
       s += (C[ab]*ab)/2;
     }
     else {
-      // on les parcourt 1 a une jusqu'a arriver a s = n
+      // on les parcourt une a une jusqu'a arriver a s > n
       for (int a=1; a<=ab/2; ++a) {
 	int b = ab-a;
 	if (a==b) continue;
@@ -102,12 +117,12 @@ int main() {
 	  if (s>n) break;
 	  ++f;
 	  s += b;
+	  if (s>n) break;
 	}
-	if (s>n) break;
       }
     }
     ++ab;
   }
-  cout << f-1 << endl;
+  cout << f << endl;
   return 0;
 }
