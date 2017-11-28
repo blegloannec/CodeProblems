61145d547163893f3c178c06e97ab67dfae0bb61
blegloannec <blg@gmx.com>
2017-11-28 23:13:04 +0100

    solv./clean. old icpc

--- ACM-ICPC/L3/Seance6/11179.cpp
+++ ACM-ICPC/L3/Seance6/11179.cpp
@@ -1,67 +1,87 @@
-/* 11179 Bit Stream
-   Ce programme calcule les checksums a la volee
-   Ce qui est trop lent quand on utilise des long long,
-   ce qui est imposé par les bornes enormes des donnees
-   du probleme.
-*/
-   
 #include <cstdio>
-#include <iostream>
+#include <vector>
 using namespace std;
 
 typedef unsigned long long ent;
 
-int b,cur_puis;
-ent n,m,cur_sum,cur_int,C,L;
-
-void hasher(int bit) { // Calcule la checksum a la volee
-  cur_int = 2*cur_int + bit;
-  if (cur_puis == 0) {
-    cur_sum = (cur_sum*n + cur_int) % m;
-    cur_int = 0;
-    cur_puis = b-1;
-  }
-  else --cur_puis;
-}
-
 int main() {
-  int cpt,in,cur_bit;
-  ent l;
-  cpt = 0;
+  int cas = 1;
 
-  while (scanf("%Ld", &L)==1) {
-    if (L==0) return 0;
-    scanf("%d %Ld %Ld %Ld", &b, &n, &m, &C);
+  while (true) {
+    // input reading
+    int b;
+    ent L,n,m,C;
+    scanf("%Ld",&L);
+    if (L==0) break;
+    scanf("%d %Ld %Ld %Ld",&b,&n,&m,&C);
 
-    cur_sum = 0;
-    cur_puis = b-1;
-    cur_int = 0;
-    l = 0;
-
-    scanf("%d", &in);
-    l += in;
-    while (--in >= 0) hasher(0);
-
-    cur_bit = 1;
-    scanf("%d", &in);
-    while (in != 0) {
-      l += in;
-      while (--in >= 0) hasher(cur_bit);
-      cur_bit = (cur_bit+1)%2;
-      scanf("%d", &in);
+    int bit = 0, r, max_chunk_nb = 2;
+    ent l = 0;
+    vector<int> BS;
+    scanf("%d",&r);
+    if (r==0) bit = 1;
+    else {
+      BS.push_back(r);
+      l += r;
+      max_chunk_nb = max(max_chunk_nb,r/b+1);
+    }
+    scanf("%d",&r);
+    while (r>0) {
+      BS.push_back(r);
+      l += r;
+      max_chunk_nb = max(max_chunk_nb,r/b+1);
+      scanf("%d",&r);
     }
 
-    if (l != L) {
-      printf("Bitstream %d: Invalid Length\n", ++cpt);
+    // size check
+    if (l!=L) {
+      printf("Bitstream %d: Invalid Length\n",cas++);
       continue;
     }
 
-    // On complete avec des 0
-    while (cur_puis != b-1) hasher(0);
-    
-    if (cur_sum == C) printf("Bitstream %d: Compression OK\n", ++cpt);
-    else printf("Bitstream %d: Invalid Checksum\n", ++cpt);
+    // computing checksum
+    // pre-computations
+    vector<ent> pown(max_chunk_nb,1), pow1(max_chunk_nb,0);
+    ent chunk1 = (1LL<<b)-1;
+    for (int i=1; i<max_chunk_nb; ++i) {
+      pown[i] = (pown[i-1]*n)%m;
+      pow1[i] = (pow1[i-1]*n + chunk1)%m;
+    }
+    // actual computation in O(nb of blocks)
+    ent c = 0, chunk = 0;
+    int s = 0;
+    for (unsigned int i=0; i<BS.size(); ++i) {
+      int rs = BS[i];
+      // completing current chunk
+      while (s<b && rs>0) {
+	chunk = (chunk<<1) | bit;
+	++s;
+	--rs;
+      }
+      if (s==b) {
+	c = (c*n + chunk)%m;
+	chunk = 0;
+	s = 0;
+      }
+      // processing all full-bit chunks of the block at once
+      if (rs>0) {
+	int p = rs/b;
+	c = (c*pown[p] + (bit ? pow1[p] : 0))%m;
+	rs %= b;
+	if (bit) chunk = (1LL<<rs)-1;
+	s = rs;
+      }
+      bit = 1-bit;
+    }
+    // final padding
+    if (s>0) {
+      chunk <<= b-s;
+      c = (c*n + chunk)%m;
+    }
 
+    // output
+    if (c==C) printf("Bitstream %d: Compression OK\n",cas++);
+    else printf("Bitstream %d: Invalid Checksum\n",cas++);
   }
 
   return 0;
