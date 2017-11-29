ced6e2c2659deacf6f30b20ea80d63850e0aef9f
blegloannec <blg@gmx.com>
2017-11-30 00:26:14 +0100

    icpc

--- ACM-ICPC/L3/Summer2008/260.cpp
+++ ACM-ICPC/L3/Summer2008/260.cpp
@@ -1,29 +1,42 @@
 // ACM 260
 #include <cstdio>
+using namespace std;
 
-#define MAX 100
+const int MAX = 200;
+int N;
+bool B[MAX][MAX];
+bool seen[MAX][MAX];
 
-bool board[MAX][MAX];
-bool visited[MAX][MAX];
-
-bool white_win() {
-  for 
+bool dfs(int i, int j) {
+  if (j==N-1) return true;
+  seen[i][j] = true;
+  for (int di=-1; di<=1; ++di)
+    if (0<=i+di && i+di<N)
+      for (int dj=-1; dj<=1; ++dj)
+	if (di+dj!=0 && 0<=j+dj && j+dj<N && B[i+di][j+dj] && !seen[i+di][j+dj])
+	  if (dfs(i+di,j+dj)) return true;
   return false;
 }
 
-bool inboard(int i, int j) {
-  
+bool white_win() {
+  for (int i=0; i<N; ++i)
+    if (B[i][0] && !seen[i][0] && dfs(i,0)) return true;
+  return false;
 }
 
 int main() {
-  int t;
-  char c;
-  
-  while (scanf("%d", &t)==1) {
-    for (int i=0; i<t; i++)
-      for (int j=0; j<t; j++) {
-	scanf("%c",c);
-	board[i][j] = (c=='W');
+  int cas = 1;
+  while (true) {
+    scanf("%d",&N);
+    if (N==0) break;
+    for (int i=0; i<N; ++i)
+      for (int j=0; j<N; ++j) {
+	char c;	
+	scanf(" %c",&c);
+	B[i][j] = (c=='w');
+	seen[i][j] = false;
       }
+    printf("%d %c\n", cas++, (white_win() ? 'W' : 'B'));
   }
+  return 0;
 }
