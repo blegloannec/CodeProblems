78d761f2b7b1c28f744fb97610f63acbcd9a9f7c
blegloannec <blg@gmx.com>
2018-02-15 15:24:37 +0100

    hr doc

--- HackerRank/nb_zero-xor_subsets.py
+++ HackerRank/nb_zero-xor_subsets.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
+# E = {1,...,2^N-1}
+# Posons B = {2^0,2^1,...,2^(N-1)} les puissances de 2 dans E
+# (i.e. les valeurs avec exactement 1 bit a 1)
+# et A = E\B.
+# Si X inclus dans E est de xor nul, alors X est parfaitement caracterise
+# par Y = X inter A, car on sait alors exactement et de facon unique completer
+# Y en X avec des elements de B (ajouter les elements correspondant aux bits
+# a 1 de xor(Y)). Il y a donc exactement autant de sous-ensembles de xor nul
+# dans E que de sous-ensembles de A (de cardinal |E|-|B| = 2^N-N)
+# i.e. 2^(2^N-N).
+
 P = 10**9+7
 Phi = P-1
 
