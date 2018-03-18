5da0d33ed09186fe6c7ed13c78ae5805bb53e3b2
blegloannec <blg@gmx.com>
2018-03-18 23:58:42 +0100

    pe

--- ProjectEuler/622.py
+++ ProjectEuler/622.py
@@ -3,6 +3,11 @@
 # http://oeis.org/A055388
 # http://oeis.org/A002326
 
+# for even n, the period is twice the multiplicative order of 4 mod odd n-1
+# hence we need 4^30 = 1 mod n-1, i.e. n-1 | 2^60-1
+# and 4^k =/= 1 mod n-1 for k < 30 = 2*3*5
+# it is enough to check that for k = 30/2, 30/3 and 30/5
+
 # primal decomposition of 2^60-1
 D = [(3,2),(5,2),(7,1),(11,1),(13,1),(31,1),(41,1),(61,1),(151,1),(331,1),(1321,1)]
 
