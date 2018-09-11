595058f81d2a6e12e74334d434cfe480730677dc
blegloannec <blg@gmx.com>
2018-09-11 13:10:21 +0200

    cg (brackets balancing dp)

---
+++ CodinGame/Community/brackets_ultimate_edition__generator__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import random
+random.seed()
+
+B = [['(','[','{','<'],[')',']','}','>']]
+
+def gen(n):
+    if n<0:
+        return []
+    n -= 1
+    b = random.randint(0,3)
+    res = [B[random.randint(0,1)][b]]
+    while n:
+        m = random.randint(1,n)
+        res += gen(m)
+        n -= m
+    res.append(B[random.randint(0,1)][b])
+    return res
+
+def main():
+    N = 10
+    print(N)
+    for _ in range(N):
+        print(''.join(gen(random.randint(100,500))))
+
+main()
