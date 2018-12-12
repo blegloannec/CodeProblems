53bb3b1e29374e92130450bd7cbfc2d627c92da4
blegloannec <blg@gmx.com>
2018-12-12 15:37:46 +0100

    aoc

--- AdventOfCode/18/day10.py
+++ AdventOfCode/18/day10.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-import sys, re
+import sys, re, os
+from PIL import Image
 
 form = re.compile('position=< *(-?\d+), *(-?\d+)> velocity=< *(-?\d+), *(-?\d+)>')
 I = [tuple(map(int,form.match(L.strip()).groups())) for L in sys.stdin.readlines()]
@@ -22,3 +23,31 @@ def pic(t, out=False):
 tmin = min(range(15000),key=pic)
 pic(tmin,True)
 print(tmin)
+
+
+# GIF anim for the fun!
+def gif_frames(t0,t1):
+    X,Y = [],[]
+    x0 = y0 = float('inf')
+    x1 = y1 = float('-inf')
+    for t in range(t0,t1+1):
+        X.append([px+t*vx for px,_,vx,_ in I])
+        Y.append([py+t*vy for _,py,_,vy in I])
+        x0,y0 = min(x0,min(X[-1])),min(y0,min(Y[-1]))
+        x1,y1 = max(x1,max(X[-1])),max(y1,max(Y[-1]))
+    W,H = x1-x0+1,y1-y0+1
+    assert(max(W,H)<=500)
+    for t in range(len(X)):
+        Img = Image.new('1',(W,H))
+        Pix = Img.load()
+        for x,y in zip(X[t],Y[t]):
+            Pix[x-x0,y-y0] = 1
+        if t==len(X)-1:
+            Img.save('gif10/last_frame.gif')
+        else:
+            Img.save('gif10/frame%03d.gif' % t)
+        Img.close()
+
+#gif_frames(tmin-30,tmin)
+# using ImageMagick to animate:
+#os.system('convert -delay 6 -loop 0 gif10/frame*.gif -delay 90 gif10/last_frame.gif anim10.gif')
---
+++ AdventOfCode/18/day12.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+# A cellular automaton today!
+
+import sys
+from PIL import Image
+
+I = [L.strip() for L in sys.stdin.readlines()]
+C0 = {i for i,c in enumerate(I[0].split(': ')[1]) if c=='#'}  # sparse conf
+R = 2      # radius
+W = 2*R+1  # width
+Rule = [False]*(1<<W)
+for L in I[2:]:
+    A,B = L.split(' => ')
+    Rule[sum(1<<i for i,c in enumerate(A) if c=='#')] = (B=='#')
+
+
+# Part 1
+def step(C):
+    D = set()
+    for x in range(min(C)-W+1,max(C)+W):
+        if Rule[sum(1<<(y-x+R) for y in range(x-R,x+R+1) if y in C)]:
+            D.add(x)
+    return D
+
+def steps(C,T):
+    for _ in range(T):
+        C = step(C)
+    return C
+
+print(sum(steps(C0,20)))
+
+
+# Part 2
+def diagram(C,T):
+    G = [C]
+    for _ in range(T):
+        C = step(C)
+        G.append(C)
+    Xmin = min(min(L) for L in G)
+    Xmax = max(max(L) for L in G)
+    W = Xmax-Xmin+1
+    H = len(G)
+    Img = Image.new('1',(W,H),1)
+    Pix = Img.load()
+    for i in range(H):
+        for j in G[i]:
+            Pix[j-Xmin,i] = 0
+    Img.save('diagram12.png')
+    Img.close()
+
+#diagram(C0,300)
+# when visualizing the diagram, we observe that after some time,
+# the configuration is a constant one that simply shifts 1 cell
+# to the right at each step
+# NB: this is consistent with the fact that the rule is actually
+# independent of the rightmost cell (this has to be the case,
+# at least for 5-patterns that can appear in the ultimately
+# shifting constant part):
+assert(R[i]==R[(1<<(W-1))|i] for i in range(1<<(W-1)))
+
+t = 300
+C = steps(C0,t)
+S = sum(C)        # constant part value at t
+N = len(C)        # nb of alive cells
+T = 50*10**9      # required large time
+print(S+N*(T-t))  # formula
