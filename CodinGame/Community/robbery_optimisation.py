2908403549844e9f994b85409e9bae4045868ee1
blegloannec <blg@gmx.com>
2019-04-22 22:08:36 +0200

    cg

---
+++ CodinGame/Community/robbery_optimisation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+N = int(input())
+H = [int(input()) for _ in range(N)]
+DP = H[:]
+if N>1:
+    DP[1] = max(H[0],H[1])
+    for i in range(2,N):
+        DP[i] = max(H[i]+DP[i-2], DP[i-1])
+print(DP[N-1])
