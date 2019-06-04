d35e23ffa161839fcfda2ee94800e3ac8a07b46d
blegloannec <blg@gmx.com>
2019-06-04 23:11:54 +0200

    cg

---
+++ CodinGame/Creations/CoC_the_sicilian_birds/README.md
@@ -0,0 +1,4 @@
+### References
+* [Post tag systems](https://en.wikipedia.org/wiki/Tag_system)
+* [Collatz conjecture](https://en.wikipedia.org/wiki/Collatz_conjecture#As_a_tag_system)
+* [L. De Mol, *Tag systems and Collatz-like functions*, TCS 2008](https://doi.org/10.1016/j.tcs.2007.10.020)
--- CodinGame/Creations/CoC_the_sicilian_birds/solution.py
+++ CodinGame/Creations/CoC_the_sicilian_birds/solution.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
-# Pb Ref: https://en.wikipedia.org/wiki/Tag_system  [L. De Mol, TCS 2008]
-
-# expected solution
+## Fastest mode?
+# Several possible implementations. A really too clumsy one might time out
+# but no tricks are required. The following is a possible / reasonable /
+# expectable solution.
 
 from collections import deque
 
@@ -20,3 +21,14 @@ while len(Q)>1:
     N += Zero[x] - (x==0) - (y==0)
     if len(Q)==N:
         print(N)
+
+## Shortest mode?
+# Compact implementations are an option. Or analyze what the birds do
+# and deduce a formula (which should be even shorter). Or guess that
+# formula empirically (as one would do in reverse mode).
+
+## No reverse mode?
+# Would be possible, yet uninteresting, unoriginal and spoils the fun IMO.
+
+## Who's Emilia?
+# She works at the Syracuse post office. In Sicilia... unless it's NY state.
