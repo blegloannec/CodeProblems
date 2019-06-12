f2ec90699425654617de50a55346a1bf2f2cf88b
blegloannec <blg@gmx.com>
2019-06-12 13:00:02 +0200

    ks (sudoku dlx)

--- Kattis/sudokunique.cpp
+++ Kattis/sudokunique.cpp
@@ -89,9 +89,14 @@ void uncover(cell *c) {
 }
 
 
-// SOLVER
-bool solve(cell *header, vector<int> &sol) {
-  if (header->R==header) return true;
+// COUNTING SOLUTIONS
+void solve_count(cell *header, vector<int> &sol_tmp, vector<int> &sol, int &sol_cnt) {
+  if (header->R==header) {
+    if (sol_cnt==0) sol = sol_tmp;
+    ++sol_cnt;
+    return;
+  }
+  if (sol_cnt>1) return;  // for this pb only
   cell *c = NULL;
   cell *j = header->R;
   while (j!=header) {
@@ -101,26 +106,25 @@ bool solve(cell *header, vector<int> &sol) {
   cover(c);
   cell *r = c->D;
   while (r!=c) {
-    sol.push_back(r->S);
+    sol_tmp.push_back(r->S);
     cell *j = r->R;
     while (j!=r) {
       cover(j->C);
       j = j->R;
     }
-    if (solve(header,sol)) return true;
+    solve_count(header,sol_tmp,sol,sol_cnt);
     j = r->L;
     while (j!=r) {
       uncover(j->C);
       j = j->L;
     }
-    sol.pop_back();
+    sol_tmp.pop_back();
     r = r->D;
   }
   uncover(c);
-  return false;
 }
 
-bool dancing_links(int size, subsets &sets, vector<int> &sol) {
+int dancing_links_count(int size, subsets &sets, vector<int> &sol) {
   cell *header = new cell(NULL,NULL,0,NULL);
   vector<cell*> col;
   for (int j=0; j<size; ++j)
@@ -132,56 +136,74 @@ bool dancing_links(int size, subsets &sets, vector<int> &sol) {
       row = new cell(row,col[*jt],i,col[*jt]);
     }
   }
-  return solve(header,sol);
+  vector<int> sol_tmp;
+  int sol_cnt = 0;
+  solve_count(header,sol_tmp,sol,sol_cnt);
+  return sol_cnt;
 }
 
 
-// COUNTING SOLUTIONS
-void solve_count(cell *header, int &sol) {
-  if (header->R==header) {
-    ++sol;
-    return;
-  }
-  //cell *c = NULL;
-  cell *c = header->R;
-  /*while (j!=header) {
-    if (c==NULL || j->S<c->S) c = j;
-    j = j->R;
-    }*/
-  cover(c);
-  cell *r = c->D;
-  while (r!=c) {
-    //sol.push_back(r->S);
-    cell *j = r->R;
-    while (j!=r) {
-      cover(j->C);
-      j = j->R;
-    }
-    solve_count(header,sol);
-    j = r->L;
-    while (j!=r) {
-      uncover(j->C);
-      j = j->L;
-    }
-    //sol.pop_back();
-    r = r->D;
-  }
-  uncover(c);
+// REDUCTION Sudoku --> Exact Cover
+typedef vector< vector<int> > grid;
+
+subset pack_subset(int i, int j, int d) {
+  subset S(4);
+  --d;                    // digit d from 1-9 to 0-8
+  int k = 3*(i/3) + j/3;  // block
+  S[0] = 9*i + j;         // cell (i,j)
+  S[1] = 9*(i+ 9) + d;    // digit d in line i
+  S[2] = 9*(j+18) + d;    // digit d in column j
+  S[3] = 9*(k+27) + d;    // digit d in block k
+  return S;
 }
 
-int dancing_links_count(int size, subsets &sets) {
-  cell *header = new cell(NULL,NULL,0,NULL);
-  vector<cell*> col;
-  for (int j=0; j<size; ++j)
-    col.push_back(new cell(header,NULL,0,NULL));
-  for (int i=0; i<(int)sets.size(); ++i) {
-    cell *row = NULL;
-    for (subset::iterator jt=sets[i].begin(); jt!=sets[i].end(); ++jt) {
-      ++(col[*jt]->S);
-      row = new cell(row,col[*jt],i,col[*jt]);
+void unpack_subset(const subset &S, int &i, int &j, int &d) {
+  i = S[0]/9;
+  j = S[0]%9;
+  d = S[1]%9 + 1;
+}
+
+int sudoku_solve(grid &G) {
+  subsets S;
+  for (int i=0; i<9; ++i)
+    for (int j=0; j<9; ++j) {
+      if (G[i][j]!=0)
+	S.push_back(pack_subset(i,j,G[i][j]));
+      else
+	for (int d=1; d<=9; ++d)
+	  S.push_back(pack_subset(i,j,d));
     }
+  int size = 4*9*9;
+  vector<int> Sol;
+  int cnt = dancing_links_count(size, S, Sol);
+  if (cnt==1)
+    for (int k: Sol) {
+      int i,j,d;
+      unpack_subset(S[k],i,j,d);
+      G[i][j] = d;
+    }
+  return cnt;
+}
+
+
+// MAIN
+int main() {
+  bool first = true;
+  grid G(9, vector<int>(9));
+  while (true) {
+    for (int i=0; i<9; ++i)
+      for (int j=0; j<9; ++j)
+	if (!(cin >> G[i][j])) return 0;
+    if (first) first = false;
+    else cout << endl;
+    int res = sudoku_solve(G);
+    if (res==1) {
+      for (int i=0; i<9; ++i)
+	for (int j=0; j<9; ++j)
+	  cout << G[i][j] << (j==8 ? '\n' : ' ');
+    }
+    else if (res==0) cout << "Find another job" << endl;
+    else cout << "Non-unique" << endl;
   }
-  int sol = 0;
-  solve_count(header,sol);
-  return sol;
+  return 0;
 }
