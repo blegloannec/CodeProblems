69f19576b63097a3bff95f4bacfca6c211e587b1
blegloannec <blg@gmx.com>
2019-09-11 23:34:19 +0200

    hr typo

--- HackerRank/accessory_collection.py
+++ HackerRank/accessory_collection.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 # Very interesting and tricky problem!
-# The simplest approach is to consider the construction of and optimal frequency array
+# The simplest approach is to consider the construction of an optimal frequency array
 # Freq (of size A and sum L) for the solution.
 # 1. To maximize the score, this array can be considered non-decreasing.
 # 2. The ">=D values among any N-subset" <=> sum_v Freq[v] < N for any (D-1)-subset of A
 #    Keeping 1. in mind, to maximize the score, the sum of the freq of the last D-1 values
 #    of A should be maximal, i.e. N-1, and (any other (D-1)-subset having a <= sum) this
 #    immediately implies that the condition is verified on the whole Freq.
-# 3. If p = A-(D-1)+1 and x = Freq[p], we shoud set
+# 3. If p = A-(D-1)+1 and x = Freq[p], we should set
 #    Freq[p:] = [x x ..(D-2 times).. x y] with y = N-1 - (D-2)*x (which must also be >=x)
 #    and Freq[:p] of the form [0 0 .. 0 r x x .. x] (summing to L-(N-1)).
 #    That Freq maximizes the score for a given x and the corresponding score can easily
