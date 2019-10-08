c6402195191ff70ba0c4525e7990989dcf3be435
blegloannec <blg@gmx.com>
2019-10-08 16:19:10 +0200

    kt

--- Kattis/zipline.py
+++ Kattis/zipline.py
@@ -5,14 +5,9 @@ from math import hypot
 T = int(input())
 for _ in range(T):
     W,G,H,R = map(int,input().split())
-    L = lambda x: hypot(G-R,x) + hypot(W-x,H-R)
-    dL = lambda x: x/hypot(G-R,x) + (x-W)/hypot(W-x,H-R)
-    xl, xr = 0, W
-    while xr-xl>1e-6:
-        xm = (xl+xr)/2.
-        if dL(xm)<0:
-            xl = xm
-        else:
-            xr = xm
-    lmin = hypot(abs(G-H),W)
-    print(lmin, L(xl))
+    G -= R
+    H -= R
+    L = lambda x: hypot(G,x) + hypot(W-x,H)
+    lmin = hypot(G-H,W)
+    lmax = lmin if H==G==0 else L(G*W/(G+H))
+    print(lmin, lmax)
