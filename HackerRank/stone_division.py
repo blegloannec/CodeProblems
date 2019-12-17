17291576f5face38bf55f7743c15f96c8d3ac267
blegloannec <blg@gmx.com>
2019-12-17 23:41:21 +0100

    hr & kt

--- HackerRank/stone_division.py
+++ HackerRank/stone_division.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 
-import sys
-sys.setrecursionlimit(5000)
+from functools import lru_cache
 
-memo = {}
-def dp(n):
-    if n not in memo:
-        res = 0
-        for s in S:
-            if s>=n:   break
-            if n%s==0: res = max(res, 1+(n//s)*dp(s))
-        memo[n] = res
-    return memo[n]
+@lru_cache(maxsize=None)
+def grundy(n):
+    g = 0
+    G = []
+    for s in S:
+        if n%s==0:
+            G.append(0 if s&1==0 else grundy(n//s))
+        while g in G:
+            g += 1
+    return g
 
-if __name__ == '__main__':
-    Q = int(input())
-    for _ in range(Q):
-        N,M = map(int,input().split())
-        S = sorted(map(int,input().split()))
-        print(dp(N))
-        memo.clear()
+def main():
+    global S
+    N,M = map(int,input().split())
+    S = list(map(int,input().split()))
+    print('First' if grundy(N)!=0 else 'Second')
+
+main()
