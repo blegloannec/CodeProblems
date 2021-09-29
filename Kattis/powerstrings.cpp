432038572c3af6a6fb6fa62b1138bd4addf244eb
blegloannec <blg@gmx.com>
2021-09-29 23:20:02 +0200

    ks

--- Kattis/powerstrings.cpp
+++ Kattis/powerstrings.cpp
@@ -30,12 +30,32 @@ int kmp(const string &A, const string &B, int i0=0) {
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
+  string S;  
+  vector<int> P;  
   while (true) {
-    string S;
     cin >> S;
     if (S==".") break;
-    int i = kmp(S+S, S, 1);
-    cout << S.size()/i << endl;
+    
+    /* 
+      Method 0: the (S+S).find(S,1) trick
+      Unfortunately using STL string find is too slow!..
+      (theoretically it should be linear, but...)
+      We have to use out own KMP.
+    */
+    //int p = kmp(S+S, S, 1);
+    
+    /*
+      Method 1: using "borders" (twice as fast in practice)
+      We do not really need KMP, the prefix array is enough.
+      Its last cell gives the size of the longest strict prefix
+      that is also a suffix of S, hence it must be:
+        length of S - shortest period
+    */
+    prefs(S,P);
+    int p = S.size()-P.back();
+    
+    // output
+    cout << S.size()/p << endl;
   }
   return 0;
 }
